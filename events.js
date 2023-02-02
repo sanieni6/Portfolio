291f525c288efd420c673dd42efb15904bcb4e01
pmaxy <84446514+pmaxy@users.noreply.github.com>
2023-02-02 10:41:11 -0600

    Add new javascript file

--- events.js
+++ events.js
@@ -1,17 +1,21 @@
-let btn = document.querySelector('#button');
-console.log(btn);
-let li_items = document.querySelectorAll('.frame-nav');
-var nav = document.querySelector(".content-nav");
-let name = document.querySelector(".name");
-let change_image =  document.querySelector('.close-image');
-let main_nav = document.querySelector('.navigator');function close() {
-    
-    let screen_size = screen.width;
-    nav.style.display = "none";
-    
-    name.style.display = 'block';
-    change_image.src = 'images/menu.png';
+const btn = document.querySelector('#button');
+const liItems = document.querySelectorAll('.frame-nav');
+const nav = document.querySelector('.content-nav');
+const name = document.querySelector('.name');
+const closeBtn = document.querySelector('#close-button');
+function displayMenu() {
+  closeBtn.style.display = 'block';
+  btn.style.display = 'none';
+  nav.classList.add('display-nav');
+  nav.classList.remove('no-nav');
+  name.style.display = 'none';
+  nav.classList.add('nav-styles');
 }
-li_items.forEach((element) => element.addEventListener('click', close));
-btn.addEventListener('click',display_menu);
-window.addEventListener('resize',recall);
+function close() {
+  nav.classList.remove('display-nav');
+  nav.classList.add('no-nav');
+  name.style.display = 'block';
+  closeBtn.style.display = 'none';
+  btn.style.display = 'block';
+}
+
---
+++ popup.js
@@ -0,0 +1,16 @@
+const btnPopup = document.querySelectorAll('.btnpop');
+const closeBtn = document.createElement('button');
+const windowShadow = document.createElement('div');
+windowShadow.className = 'window';
+const popUp = document.createElement('div');
+const title = document.createElement('h2');
+const divHead = document.createElement('div');
+const divTitle = document.createElement('div');
+const divMiddle = document.createElement('div');
+const divFooter = document.createElement('div');
+const imagePortrait = document.createElement('img');
+const pLoret = document.createElement('p');
+const buttonContainer = document.createElement('div');
+const btnLive = document.createElement('a');
+const btnSee = document.createElement('a');
+const body = document.querySelector('body');
