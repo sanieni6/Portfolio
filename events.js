33ea892517e0f957a900935b7b66d5b665fdd5a3
sanieni6 <sanieni6@gmail.com>
2023-02-02 16:15:57 -0500

    solve merge problem

--- events.js
+++ events.js
@@ -16,6 +16,7 @@ function close() {
   nav.classList.add('no-nav');
   name.style.display = 'block';
   closeBtnEvent.style.display = 'none';
+  btn.style.display = 'block';
 }
 liItems.forEach((element) => element.addEventListener('click', close));
 btn.addEventListener('click', displayMenu);
