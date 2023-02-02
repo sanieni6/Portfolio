a2df05cb505fbcd8d43f25bb8a6aaf4de21b805f
sanieni6 <sanieni6@gmail.com>
2023-02-02 15:24:17 -0500

    Solve tipyng problems

--- events.js
+++ events.js
@@ -2,9 +2,9 @@ const btn = document.querySelector('#button');
 const liItems = document.querySelectorAll('.frame-nav');
 const nav = document.querySelector('.content-nav');
 const name = document.querySelector('.name');
-const closeBtn = document.querySelector('#close-button');
+const closeBtnEvent = document.querySelector('#close-button');
 function displayMenu() {
-  closeBtn.style.display = 'block';
+  closeBtnEvent.style.display = 'block';
   btn.style.display = 'none';
   nav.classList.add('display-nav');
   nav.classList.remove('no-nav');
@@ -15,10 +15,9 @@ function close() {
   nav.classList.remove('display-nav');
   nav.classList.add('no-nav');
   name.style.display = 'block';
-  closeBtn.style.display = 'none';
+  closeBtnEvent.style.display = 'none';
   btn.style.display = 'block';
 }
-
 liItems.forEach((element) => element.addEventListener('click', close));
 btn.addEventListener('click', displayMenu);
-closeBtn.addEventListener('click', close);
+closeBtnEvent.addEventListener('click', close);
