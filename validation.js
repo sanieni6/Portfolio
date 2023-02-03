e398077a432da389d6fa31a2fab0f46c7887c041
sanieni6 <sanieni6@gmail.com>
2023-02-03 10:17:40 -0500

    Add validation feature

--- validation.js
+++ validation.js
@@ -4,3 +4,13 @@ const message = document.createElement('span');
 message.classList.add('error-message');
 message.textContent = '*Please, the email address must be in lowercase';
 errorMessage.appendChild(message);
+document.getElementById('forma').addEventListener('submit', (event) => {
+  const email = emailInput.value;
+  if (email.toLowerCase() !== email) {
+    emailInput.style.border = '3px solid red';
+    message.style.display = 'block';
+    event.preventDefault();
+  } else {
+    message.style.display = 'none';
+  }
+});
