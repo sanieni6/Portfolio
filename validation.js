58999ea7721d303e5ca99f3002b33b9353d15e90
sanieni6 <sanieni6@gmail.com>
2023-02-03 13:59:51 -0500

    Add feature to preserve data

--- validation.js
+++ validation.js
@@ -1,9 +1,43 @@
+// validation
 const emailInput = document.getElementById('user-email');
 const errorMessage = document.querySelector('.validation');
 const message = document.createElement('span');
 message.classList.add('error-message');
 message.textContent = '*Please, the email address must be in lowercase';
 errorMessage.appendChild(message);
+// validation
+
+// preserve data
+const userName = document.getElementById('username');
+const descriptionTextarea = document.getElementById('msg-input');
+const validate = document.querySelectorAll('.input');
+const formData = {
+  userName: JSON.parse(localStorage.getItem('formData')).userName,
+  userEmail: JSON.parse(localStorage.getItem('formData')).userEmail,
+  message: JSON.parse(localStorage.getItem('formData')).message,
+};
+
+const renderInfo = () => {
+  const storedUserData = JSON.parse(localStorage.getItem('formData'));
+  userName.value = storedUserData.userName;
+  emailInput.value = storedUserData.userEmail;
+  descriptionTextarea.value = storedUserData.message;
+};
+const local = (event, value) => {
+  formData[value] = event.target.value;
+  localStorage.setItem('formData', JSON.stringify(formData));
+};
+window.onload = renderInfo;
+validate.forEach((element) => element.addEventListener('keyup', (e) => {
+  local(e, element.name);
+}));
+
+
+
+
+// preserve data
+
+// validation
 document.getElementById('forma').addEventListener('submit', (event) => {
   const email = emailInput.value;
   if (email.toLowerCase() !== email) {
@@ -14,3 +48,4 @@ document.getElementById('forma').addEventListener('submit', (event) => {
     message.style.display = 'none';
   }
 });
+// validation
