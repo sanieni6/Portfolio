ba3a162f6bb24088850a9ee6cdfd091b903660da
sanieni6 <sanieni6@gmail.com>
2023-02-02 22:49:16 -0500

    Validate the mail input

--- validation.js
+++ validation.js
@@ -1,25 +1,17 @@
 const emailInput = document.getElementById('user-email');
-const buttonForm = document.getElementById('button-form');
 const errorMessage = document.querySelector('.validation');
 const message = document.createElement('span');
 message.classList.add('error-message');
-message.textContent = 'Format error';
+message.textContent = '*Please, the email address must be in lowercase';
 errorMessage.appendChild(message);
 // function validateEmail() {
 document.getElementById('forma').addEventListener('submit', (event) => {
   const email = emailInput.value;
   if (email.toLowerCase() !== email) {
-    // Mostrar un mensaje de error si el correo electrónico no está en minúsculas
+    emailInput.style.border = '3px solid red';
     message.style.display = 'block';
     event.preventDefault();
-    // emailInput.setCustomValidity("El correo electrónico debe estar en minúsculas");
   } else {
-    // Limpiar el mensaje de error si el correo electrónico es válido
-    // emailInput.setCustomValidity("");
     message.style.display = 'none';
   }
 });
-
-// emailInput.addEventListener('input', validateEmail);
-// buttonForm.addEventListener('click', validateEmail(event));
-// document.getElementById("form").addEventListener("submit", function(event) {
